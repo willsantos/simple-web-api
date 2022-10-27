4460f46e4f26223d6bafd7f2fc7dc71718988594
Wilson Santos <contato@wilsonsantos.net>
2022-10-27 16:06:36 -0300

    :white_check_mark: Add Injeção de dependencias
    Add as config de injeção de dependencia
    Add TestMethod Cadastra usuario com sucesso

---
+++ Wilson.WebApi.Test/Repositories/UsuarioRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using ExercicioAPIStella.Data.Context;
+using ExercicioAPIStella.Data.Entities;
+using ExercicioAPIStella.Data.Repositories;
+using ExercicioAPIStella.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wilson.WebApi.Test.Repositories;
+
+[TestClass]
+public class UsuarioRepositoryTest
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly IConfiguration _configuration;
+
+
+    public UsuarioRepositoryTest()
+    {
+        
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped<IUsuarioRepository, UsuarioRepository>();
+        serviceCollection.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlServer(@"Server=127.0.0.1,1439;Database=stellaApi_tests;User=SA;Password=wd%!r$H7Ez@yuPz*sx*3wUgBcwv;")
+            );
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+        _usuarioRepository = serviceProvider.GetRequiredService<IUsuarioRepository>();
+    }
+
+    [TestMethod]
+    public async Task CadastraUsuarioComSucessoTest()
+    {
+        var novoUsuario = new Usuario()
+        {
+            Nome = "Leonor Valente",
+            Telefone = "794-639-4982",
+            Email = "Leonor.Valente71@hotmail.com",
+            CPF = "024.516.478-25",
+            Senha = "f?(Yk!|xHOlXr#g%1|z"
+        };
+
+
+        await _usuarioRepository.AddAsync(novoUsuario);
+        var usuarioAssert = await _usuarioRepository.FindAsync(novoUsuario.UsuarioId);
+
+        Assert.AreEqual(usuarioAssert, novoUsuario);
+    }
+}
